a2413e23a79d0a05ab6e1293e682fca3e3dc1ff1
ksdyb.park <ksdyb@amazon.com>
2023-07-31 19:43:03 +0900

    update

--- cdk-chatbot-with-rag/lib/cdk-chatbot-with-rag-stack.ts
+++ cdk-chatbot-with-rag/lib/cdk-chatbot-with-rag-stack.ts
@@ -140,7 +140,7 @@ export class CdkChatbotWithRagStack extends cdk.Stack {
       accessPolicies: [OpenSearchAccessPolicy],      
       ebs: {
         volumeSize: 100,
-        volumeType: ec2.EbsDeviceVolumeType.PROVISIONED_IOPS_SSD_IO2,
+        volumeType: ec2.EbsDeviceVolumeType.PROVISIONED_IOPS_SSD,
       },
       nodeToNodeEncryption: true,
       encryptionAtRest: {
