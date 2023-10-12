d04ecb28d926d5f89284fb3ec61c780a3b0d9145
ksdyb.park <ksdyb@amazon.com>
2023-10-13 07:19:18 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -380,7 +380,7 @@ def get_reference(docs):
         url = doc.metadata['url']
     
         #reference = reference + (str(page)+'page in '+name+' ('+url+')'+'\n')
-        reference = reference + f"({page}page in <a href={url}>{name}</a>\n"
+        reference = reference + f"({page}page in <a href={url}>{name} target=\"_blank\"</a>\n"
         
     return reference
 
