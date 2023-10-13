3e09d44b9c8587678b302b4801443344c628aeeb
ksdyb.park <ksdyb@amazon.com>
2023-10-13 22:28:11 +0900

    update

--- cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
+++ cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
@@ -25,7 +25,7 @@ const projectName = `korean-chatbot`;
 const bucketName = `storage-for-${projectName}-${region}`; 
 const bedrock_region = "us-east-1";  // "us-east-1" "us-west-2" 
 
-const rag_type = 'opensearch';  // faiss, opensearch
+const rag_type = 'kendra';  // faiss, opensearch
 let deployed_rag_type = 'all';   // all, opensearch, kendra, faiss
 
 const opensearch_account = "admin";
