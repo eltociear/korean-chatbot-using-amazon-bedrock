633dc660a167ddd76d480583914de7b8594b5bdb
ksdyb.park <ksdyb@amazon.com>
2023-10-27 11:05:22 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -264,7 +264,7 @@ def get_prompt_template(query, convType):
             {input}
             </article>
 
-            Please identify any grammatical errors in the article. Also, add the fixed article in <result> tages at the end of answer.
+            Please identify any grammatical errors in the article. Also, add the fixed article in the end of answer. Put it in <result> tags.
             
             Assistant: """
 
