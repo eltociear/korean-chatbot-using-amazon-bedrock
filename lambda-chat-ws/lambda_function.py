7d78885110a217a980107adfb668a8c27a41433a
ksdyb.park <ksdyb@amazon.com>
2023-11-18 20:05:23 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -763,7 +763,7 @@ def get_reference(docs, rag_type):
 
             confidence = doc['confidence']
             if doc['metadata']['type'] == "QUESTION_ANSWER":
-                excerpt = doc['metadata']['excerpt']                
+                excerpt = str(doc['metadata']['excerpt'])
                 reference = reference + f"{number}. <a href=\"#\" onClick=\"alert('{excerpt}')\" target=_blank>FAQ ({confidence})</a>\n"
                 #reference = reference + f"{number}. <a href=\"#\" onClick=\"<html><body><p>{excerpt}</p></body></html>\" target=_blank>FAQ ({confidence})</a>\n"
 
