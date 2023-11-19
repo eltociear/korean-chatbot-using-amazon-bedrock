7d9a9dd40d784298b64e852dafdd1a626db2f066
ksdyb.park <ksdyb@amazon.com>
2023-11-19 13:16:05 +0900

    updte

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1086,8 +1086,8 @@ def getResponse(connectionId, jsonBody):
                 debugMessageMode = 'false'
                 msg  = "Debug messages will not be delivered to the client."
             elif text == 'clearMemory':
-                map_chat[userId] = ""
-                memory_chat = ConversationBufferMemory(human_prefix='Human', ai_prefix='Assistant')
+                memory_chat.clear()
+                # memory_chat = ConversationBufferMemory(human_prefix='Human', ai_prefix='Assistant')
                 map_chat[userId] = memory_chat
                 print('initiate the chat memory!')
                 msg  = "The chat memory was intialized in this session."
