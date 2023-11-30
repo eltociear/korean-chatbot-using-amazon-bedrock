1bb5f7399b2405f7906c6ee65cfdcdab3ae1f3d0
ksdyb.park <ksdyb@amazon.com>
2023-11-30 20:17:12 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -877,7 +877,7 @@ def extract_relevant_doc_for_kendra(query_id, apiType, query_result):
                 if attribute["Key"] == "QuestionText":
                     question_text = str(attribute["Value"]["TextWithHighlightsValue"]["Text"])
             answer = query_result["DocumentExcerpt"]["Text"]
-            excerpt = f"{question_text}: {answer}"
+            excerpt = f"Human: {question_text}\nAssistant: {answer}"
             excerpt = excerpt.replace("\n"," ") 
         else: 
             excerpt = query_result["DocumentExcerpt"]["Text"]
