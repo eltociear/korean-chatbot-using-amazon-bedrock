e77f68876b7665287595dcc4349c7f3b1e104470
ksdyb.park <ksdyb@amazon.com>
2023-12-05 18:38:15 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -249,6 +249,61 @@ def get_prompt_template(query, convType):
 
             Assistant:"""      
 
+
+        #prompt_template = """\n\nHuman: 아래 <text>는 문서에서 추출한 텍스트입니다. 친절한 AI Assistant로서 아래와 같이 정리해주세요.
+        
+        #- 50자 미안의 제목을 <title>Name: </title> 안에 넣을것
+        #- 300자 미안의 설명을 <description>설명: </description> 안에 넣을것
+        #- 500자 미만의 내용 요약을 <summarization>요약: </summarization> 안에 넣을것
+        #- 10자 미안의 애용과 과련된 테그 5개를 <tag></tag> 테그 안에 생성할 것
+
+        #모든 생성 결과는 한국어로 해주세요. 결과에 개행문자인 "\m"과 글자 수와 같은 부가정보는 절대 포함하지 마세요.
+       # 생성이 어렵거나 해당 내용을 모르는 경우 "None"로 결과를 생성하세요.
+
+        #<text>
+        #{text}
+        #</text
+
+        #prompt_template = """\n\nHuman: 아래의 <text></text>을 시간을 포함한 문자열입니다.             
+        #    <example>
+        #    2023년 12월 5일 18시 26분
+        #    </example>
+        #    <result>
+        #    year: 2023
+        #    month: 12
+        #    day: 5
+        #    hour: 18
+        #    minute: 26
+        #    </result>
+         #   아래의 <review>에 대해서 위의 <result> 예시처럼 Extracted Topic and sentiment 을 만들어 주세요..
+
+        elif convType == "timestamp-extraction":  # Child Conversation (few shot)
+            prompt_template = """\n\nHuman: 아래의 <text></text>을 시간을 포함한 텍스트입니다. 친절한 AI Assistant로서 시간을 추출하여 아래의 <example></example>과 같이 정리해주세요.
+            
+            - 년도를 추출해서 <year>/<year>로 넣을것 
+            - 월을 추출해서 <month>/<month>로 넣을것
+            - 일을 추출해서 <day>/<day>로 넣을것
+            - 시간을 추출해서 <hour>/<hour>로 넣을것, 이때 시간은 24H으로 표시할것
+            - 분을 추출해서 <minute>/<minute>로 넣을것
+
+            이때의 예제는 아래와 같습니다.
+            <example>
+            2023년 12월 5일 18시 26분
+            </example>
+            <result>
+                <year>2023</year>
+                <month>12</month>
+                <day>05</day>
+                <hour>18</hour>
+                <minute>26</minute>
+            </result>            
+
+            <text>
+            {input}
+            </text>
+
+            Assistant:"""  
+
         elif convType == "funny": # for free conversation
             prompt_template = """\n\nHuman: 다음의 <history>는 Human과 Assistant의 친근한 이전 대화입니다. 모든 대화는 반말로하여야 합니다. Assistant의 이름은 서서이고 10살 여자 어린이 상상력이 풍부하고 재미있는 대화를 합니다. 때로는 바보같은 답변을 해서 재미있게 해줍니다.
 
@@ -610,7 +665,7 @@ def get_summary(llm, texts):
         - 10자 미안의 애용과 과련된 테그 5개를 <tag></tag> 테그 안에 생성할 것
 
         모든 생성 결과는 한국어로 해주세요. 결과에 개행문자인 "\m"과 글자 수와 같은 부가정보는 절대 포함하지 마세요.
-        생성이 어렵거나 해당 내용을 모른느 경우 "None"로 결과를 생성하세요.
+        생성이 어렵거나 해당 내용을 모르는 경우 "None"로 결과를 생성하세요.
 
         <text>
         {text}
