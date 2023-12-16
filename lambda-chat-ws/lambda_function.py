9934e1d85e5275cd54970b88720a773008cd22e1
ksdyb.park <ksdyb@amazon.com>
2023-12-16 12:42:00 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -148,8 +148,7 @@ def get_prompt_template(query, conv_type, rag_type):
             <question>            
             {input}
             </question>
-            
-            Assistant:"""
+            \n\nAssistant:"""
         elif conv_type=='qa' and rag_type=='faiss' and isReady==False: # for General Conversation
             prompt_template = """\n\nHuman: 다음의 <history>는 Human과 Assistant의 친근한 이전 대화입니다. Assistant은 상황에 맞는 구체적인 세부 정보를 충분히 제공합니다. Assistant의 이름은 서연이고, 모르는 질문을 받으면 솔직히 모른다고 말합니다.
 
