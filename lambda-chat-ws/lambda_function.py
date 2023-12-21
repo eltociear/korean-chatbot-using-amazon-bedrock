21af79b2f8cc0ee00798248d1dfd6730aa28295b
ksdyb.park <ksdyb@amazon.com>
2023-12-21 21:35:17 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -524,13 +524,12 @@ def delete_index_if_exist(index_name):
         print('no index: ', index_name)
 
 def store_document_for_opensearch(bedrock_embeddings, docs, userId, documentId):
-    documentId = documentId.replace(' ', '_') # remove spaces
-    documentId = documentId.replace(',', '_') # remove commas
-    documentId = documentId.lower() # change to lowercase
-
     index_name = "rag-index-"+userId+'-'+documentId
+    index_name = index_name.replace(' ', '_') # remove spaces
+    index_name = index_name.replace(',', '_') # remove commas
+    index_name = index_name.lower() # change to lowercase
     print('index_name: ', index_name)
-
+    
     delete_index_if_exist(index_name)
 
     try:
