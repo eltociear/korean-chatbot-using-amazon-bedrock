41b7e0c153dc30a9d1a3691d4f2623adc2cdcc29
ksdyb.park <ksdyb@amazon.com>
2023-12-22 21:12:38 +0900

    update

--- lambda-s3-event/lambda_function.py
+++ lambda-s3-event/lambda_function.py
@@ -1,10 +1,7 @@
 import json
 import boto3
 import os
-from io import BytesIO
 import traceback
-from urllib import parse
-
 from botocore.config import Config
 
 s3 = boto3.client('s3')
@@ -19,7 +16,7 @@ opensearch_url = os.environ.get('opensearch_url')
 doc_prefix = s3_prefix+'/'
 
 kendraIndex = os.environ.get('kendraIndex')
-roleArn = os.environ.get('roleArn')
+#roleArn = os.environ.get('roleArn')
 
 from opensearchpy import OpenSearch
 def delete_index_if_exist(index_name):
@@ -45,20 +42,75 @@ def delete_index_if_exist(index_name):
     else:
         print('no index: ', index_name)
 
-def store_document_for_opensearch(bedrock_embeddings, docs, userId, documentId):
-    index_name = "rag-index-"+userId+'-'+documentId
-    index_name = index_name.replace(' ', '_') # remove spaces
-    index_name = index_name.replace(',', '_') # remove commas
-    index_name = index_name.lower() # change to lowercase
-    print('index_name: ', index_name)
-    
-    delete_index_if_exist(index_name)
-
-    print('uploaded into opensearch')
-    
 # load csv documents from s3
 def lambda_handler(event, context):
     print('event: ', event)
+
+    documentIds = []
+    for record in event['Records']:
+        bucket = record['s3']['bucket']['name']
+        key = record['s3']['object']['key']
+        print('bucket: ', bucket)
+        print('key: ', key)
+
+        # get metadata from s3
+        metadata_key = meta_prefix+key
+        print('metadata_key: ', metadata_key)
+        metadata_obj = s3.get_object(Bucket=bucket, Key=metadata_key)
+        metadata_body = metadata_obj['Body'].read().decode('utf-8')
+        metadata = json.loads(metadata_body)
+        print('metadata: ', metadata)
+        userId = metadata['userId']
+        documentId = metadata['documentId']
+        print('userId: ', userId)
+        print('documentId: ', documentId)
+
+        documentIds.append(documentId)
+
+        # delete metadata
+        try: 
+            result = s3.delete_object(Bucket=bucket, Key=metadata_key)
+            print('result to delete metadata in S3: ', result)
+        except Exception:
+            err_msg = traceback.format_exc()
+            print('err_msg: ', err_msg)
+            raise Exception ("Not able to delete documents in Kendra")
+  
+        # delete index of opensearch
+        userId = 'kyopark'
+        index_name = "rag-index-"+userId+'-'+documentId
+        # index_name = "rag-index-"+documentId
+        index_name = index_name.replace(' ', '_') # remove spaces
+        index_name = index_name.replace(',', '_') # remove commas
+        index_name = index_name.lower() # change to lowercase
+        print('index_name: ', index_name)
+
+        delete_index_if_exist(index_name)
+
+    # Kendra
+    kendra_client = boto3.client(
+        service_name='kendra', 
+        region_name=kendra_region,
+        config = Config(
+            retries=dict(
+                max_attempts=10
+            )
+        )
+    )
+
+    try: 
+        result = kendra_client.batch_delete_document(
+            IndexId = kendraIndex,
+            DocumentIdList=[
+                documentId,
+            ]
+        )
+        print('result: ', result)
+    except Exception:
+        err_msg = traceback.format_exc()
+        print('err_msg: ', err_msg)
+        raise Exception ("Not able to delete documents in Kendra")
+
     
     return {
         'statusCode': 200
