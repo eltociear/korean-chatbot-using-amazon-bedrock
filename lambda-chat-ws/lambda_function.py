64965a384e0a2406488782866bea1a1bd30ae058
ksdyb.park <ksdyb@amazon.com>
2023-12-24 11:17:26 +0900

    update

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -1713,7 +1713,7 @@ def translate_relevant_documents_using_parallel_processing(docs):
         processes.append(process)
 
         n = n + 1
-        if n == len(multi_llm):
+        if n == len(llm):
             n = 0
 
     for process in processes:
