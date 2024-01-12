dddde6a37c53e78209eac4d129633e0135cb9c9e
ksdyb.park <ksdyb@amazon.com>
2024-01-12 09:29:54 +0900

    update

--- lambda-s3-event/lambda_function.py
+++ lambda-s3-event/lambda_function.py
@@ -2,8 +2,18 @@ import json
 import boto3
 import os
 import traceback
+import PyPDF2
+import time
+import docx
+
+from io import BytesIO
+from urllib import parse
 from botocore.config import Config
 from urllib.parse import unquote_plus
+from langchain.embeddings import BedrockEmbeddings
+from langchain.vectorstores.opensearch_vector_search import OpenSearchVectorSearch
+from langchain.docstore.document import Document
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 
 s3 = boto3.client('s3')
 s3_bucket = os.environ.get('s3_bucket') # bucket name
@@ -14,6 +24,14 @@ opensearch_account = os.environ.get('opensearch_account')
 opensearch_passwd = os.environ.get('opensearch_passwd')
 opensearch_url = os.environ.get('opensearch_url')
 kendraIndex = os.environ.get('kendraIndex')
+
+s3_prefix = os.environ.get('s3_prefix')
+roleArn = os.environ.get('roleArn') 
+path = os.environ.get('path')
+doc_prefix = s3_prefix+'/'
+
+capabilities = json.loads(os.environ.get('capabilities'))
+print('capabilities: ', capabilities)
 
 from opensearchpy import OpenSearch
 def delete_index_if_exist(index_name):
@@ -50,56 +68,266 @@ kendra_client = boto3.client(
     )
 )
 
+# embedding for RAG
+bedrock_region = "us-west-2"
+    
+boto3_bedrock = boto3.client(
+    service_name='bedrock-runtime',
+    region_name=bedrock_region,
+    config=Config(
+        retries = {
+            'max_attempts': 30
+        }            
+    )
+)
+
+bedrock_embeddings = BedrockEmbeddings(
+    client=boto3_bedrock,
+    region_name = bedrock_region,
+    model_id = 'amazon.titan-embed-text-v1' 
+)   
+
+def store_document_for_opensearch(bedrock_embeddings, docs, documentId):
+    index_name = "rag-index-"+documentId
+    print('index_name: ', index_name)
+    
+    delete_index_if_exist(index_name)
+
+    try:
+        new_vectorstore = OpenSearchVectorSearch(
+            index_name=index_name,  
+            is_aoss = False,
+            #engine="faiss",  # default: nmslib
+            embedding_function = bedrock_embeddings,
+            opensearch_url = opensearch_url,
+            http_auth=(opensearch_account, opensearch_passwd),
+        )
+        response = new_vectorstore.add_documents(docs)
+        print('response of adding documents: ', response)
+    except Exception:
+        err_msg = traceback.format_exc()
+        print('error message: ', err_msg)                
+        raise Exception ("Not able to request to LLM")
+
+    print('uploaded into opensearch')
+ 
+# store document into Kendra
+def store_document_for_kendra(path, doc_prefix, s3_file_name, documentId):
+    print('store document into kendra')
+    encoded_name = parse.quote(s3_file_name)
+    source_uri = path+doc_prefix+encoded_name    
+    #print('source_uri: ', source_uri)
+    ext = (s3_file_name[s3_file_name.rfind('.')+1:len(s3_file_name)]).upper()
+    print('ext: ', ext)
+
+    # PLAIN_TEXT, XSLT, MS_WORD, RTF, CSV, JSON, HTML, PDF, PPT, MD, XML, MS_EXCEL
+    if(ext == 'PPTX'):
+        file_type = 'PPT'
+    elif(ext == 'TXT'):
+        file_type = 'PLAIN_TEXT'         
+    elif(ext == 'XLS' or ext == 'XLSX'):
+        file_type = 'MS_EXCEL'      
+    elif(ext == 'DOC' or ext == 'DOCX'):
+        file_type = 'MS_WORD'
+    else:
+        file_type = ext
+
+    kendra_client = boto3.client(
+        service_name='kendra', 
+        region_name=kendra_region,
+        config = Config(
+            retries=dict(
+                max_attempts=10
+            )
+        )
+    )
+
+    documents = [
+        {
+            "Id": documentId,
+            "Title": s3_file_name,
+            "S3Path": {
+                "Bucket": s3_bucket,
+                "Key": s3_prefix+'/'+s3_file_name
+            },
+            "Attributes": [
+                {
+                    "Key": '_source_uri',
+                    'Value': {
+                        'StringValue': source_uri
+                    }
+                },
+                {
+                    "Key": '_language_code',
+                    'Value': {
+                        'StringValue': "ko"
+                    }
+                },
+            ],
+            "ContentType": file_type
+        }
+    ]
+    print('document info: ', documents)
+
+    result = kendra_client.batch_put_document(
+        IndexId = kendraIndex,
+        RoleArn = roleArn,
+        Documents = documents       
+    )
+    # print('batch_put_document(kendra): ', result)
+    print('uploaded into kendra')
+
+# load documents from s3 for pdf and txt
+def load_document(file_type, s3_file_name):
+    s3r = boto3.resource("s3")
+    doc = s3r.Object(s3_bucket, s3_prefix+'/'+s3_file_name)
+    
+    if file_type == 'pdf':
+        Byte_contents = doc.get()['Body'].read()
+        reader = PyPDF2.PdfReader(BytesIO(Byte_contents))
+        
+        texts = []
+        for page in reader.pages:
+            texts.append(page.extract_text())
+        contents = '\n'.join(texts)
+        
+    elif file_type == 'pptx':
+        Byte_contents = doc.get()['Body'].read()
+            
+        from pptx import Presentation
+        prs = Presentation(BytesIO(Byte_contents))
+
+        texts = []
+        for i, slide in enumerate(prs.slides):
+            text = ""
+            for shape in slide.shapes:
+                if shape.has_text_frame:
+                    text = text + shape.text
+            texts.append(text)
+        contents = '\n'.join(texts)
+        
+    elif file_type == 'txt':        
+        contents = doc.get()['Body'].read().decode('utf-8')
+
+    elif file_type == 'docx':
+        Byte_contents = doc.get()['Body'].read()                    
+        doc_contents =docx.Document(BytesIO(Byte_contents))
+
+        texts = []
+        for i, para in enumerate(doc_contents.paragraphs):
+            if(para.text):
+                texts.append(para.text)
+                # print(f"{i}: {para.text}")        
+        contents = '\n'.join(texts)
+            
+    # print('contents: ', contents)
+    new_contents = str(contents).replace("\n"," ") 
+    print('length: ', len(new_contents))
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+        separators=["\n\n", "\n", ".", " ", ""],
+        length_function = len,
+    ) 
+
+    texts = text_splitter.split_text(new_contents) 
+                
+    return texts
+    
 # load csv documents from s3
 def lambda_handler(event, context):
     print('event: ', event)
-
+    
     documentIds = []
     for record in event['Records']:
-        bucket = record['s3']['bucket']['name']
-        # translate utf8
-        key = unquote_plus(record['s3']['object']['key']) # url decoding
-        print('bucket: ', bucket)
-        print('key: ', key)
+        if record['eventName'] == 'ObjectRemoved:Delete':            
+            bucket = record['s3']['bucket']['name']
+            # translate utf8
+            key = unquote_plus(record['s3']['object']['key']) # url decoding
+            print('bucket: ', bucket)
+            print('key: ', key)
 
-        # get metadata from s3
-        metadata_key = meta_prefix+key+'.metadata.json'
-        print('metadata_key: ', metadata_key)
+            # get metadata from s3
+            metadata_key = meta_prefix+key+'.metadata.json'
+            print('metadata_key: ', metadata_key)
 
-        metadata_obj = s3.get_object(Bucket=bucket, Key=metadata_key)
-        metadata_body = metadata_obj['Body'].read().decode('utf-8')
-        metadata = json.loads(metadata_body)
-        print('metadata: ', metadata)
-        documentId = metadata['DocumentId']
-        print('documentId: ', documentId)
-        documentIds.append(documentId)
+            metadata_obj = s3.get_object(Bucket=bucket, Key=metadata_key)
+            metadata_body = metadata_obj['Body'].read().decode('utf-8')
+            metadata = json.loads(metadata_body)
+            print('metadata: ', metadata)
+            documentId = metadata['DocumentId']
+            print('documentId: ', documentId)
+            documentIds.append(documentId)
 
-        # delete metadata
-        print('delete metadata: ', metadata_key)
-        try: 
-            result = s3.delete_object(Bucket=bucket, Key=metadata_key)
-            # print('result of metadata deletion: ', result)
-        except Exception:
-            err_msg = traceback.format_exc()
-            print('err_msg: ', err_msg)
-            raise Exception ("Not able to delete documents in Kendra")
-  
-        # delete document index of opensearch
-        index_name = "rag-index-"+documentId
-        # print('index_name: ', index_name)
-        delete_index_if_exist(index_name)
+            # delete metadata
+            print('delete metadata: ', metadata_key)
+            try: 
+                result = s3.delete_object(Bucket=bucket, Key=metadata_key)
+                # print('result of metadata deletion: ', result)
+            except Exception:
+                err_msg = traceback.format_exc()
+                print('err_msg: ', err_msg)
+                raise Exception ("Not able to delete documents in Kendra")
+    
+            # delete document index of opensearch
+            index_name = "rag-index-"+documentId
+            # print('index_name: ', index_name)
+            delete_index_if_exist(index_name)
+            
+            # delete kendra documents
+            print('delete kendra documents: ', documentIds)
+        elif record['eventName'] != "ObjectCreated:Put":
+            bucket = record['s3']['bucket']['name']
+            # translate utf8
+            key = unquote_plus(record['s3']['object']['key']) # url decoding
+            print('bucket: ', bucket)
+            print('key: ', key)
+            
+            start_time = time.time()
+                
+            category = "upload"
+            documentId = category + "-" + key
+            documentId = documentId.replace(' ', '_') # remove spaces
+            documentId = documentId.replace(',', '_') # remove commas
+            documentId = documentId.lower() # change to lowercase
+            print('documentId: ', documentId)
+            
+            file_type = key[key.rfind('.')+1:len(key)]            
+            print('file_type: ', file_type)
+            
+            for type in capabilities:                
+                if type=='kendra':         
+                    print('upload to kendra: ', key)                            
+                    store_document_for_kendra(path, doc_prefix, key, documentId)  # store the object into kendra
+                elif type=='opensearch':
+                    if file_type == 'pdf' or file_type == 'txt' or file_type == 'csv' or file_type == 'pptx' or file_type == 'docx':
+                        print('upload to opensearch: ', key) 
+                        texts = load_document(file_type, key)
 
-    # delete kendra documents
-    print('delete kendra documents: ', documentIds)
+                        docs = []
+                        for i in range(len(texts)):
+                            docs.append(
+                                Document(
+                                    page_content=texts[i],
+                                    metadata={
+                                        'name': key,
+                                        # 'page':i+1,
+                                        'uri': path+doc_prefix+parse.quote(key)
+                                    }
+                                )
+                            )
+                        print('docs[0]: ', docs[0])    
+                        print('docs size: ', len(docs))
+                    
+                        store_document_for_opensearch(bedrock_embeddings, docs, documentId)
+        
     try: 
         result = kendra_client.batch_delete_document(
             IndexId = kendraIndex,
             DocumentIdList=[
                 documentId,
-            ],
-            #DataSourceSyncJobMetricTarget={
-            #    'DataSourceId': '850d68bd-464e-4831-bc4a-ccc8c59d8fe1'
-            #}
+            ]
         )
         print('result: ', result)
     except Exception:
