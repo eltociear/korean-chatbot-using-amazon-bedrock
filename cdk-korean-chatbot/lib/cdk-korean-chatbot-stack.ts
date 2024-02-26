92308f89967191c4baf01d086e9a480edcd6dda3
ksdyb.park <ksdyb@amazon.com>
2024-02-26 15:22:10 +0900

    update

--- cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
+++ cdk-korean-chatbot/lib/cdk-korean-chatbot-stack.ts
@@ -94,7 +94,7 @@ export class CdkKoreanChatbotStack extends cdk.Stack {
     
     // s3 
     const s3Bucket = new s3.Bucket(this, `storage-${projectName}`,{
-      bucketName: bucketName,
+      // bucketName: bucketName,
       blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
