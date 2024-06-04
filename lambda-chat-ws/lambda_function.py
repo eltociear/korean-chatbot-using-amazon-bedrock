6ce17ecd42e036c230312b0886e061435071c5f8
Kyoungsu Park <kyopark2014@gmail.com>
2024-06-04 16:33:50 +0900

    update - chunking

--- lambda-chat-ws/lambda_function.py
+++ lambda-chat-ws/lambda_function.py
@@ -2046,15 +2046,16 @@ def retrieve_docs_from_vectorstore(vectorstore_opensearch, query, top_k, rag_typ
             relevant_documents = []
             docList = []
             for re in result:
-                parent_doc_id = re[0].metadata['parent_doc_id']
-                doc_level = re[0].metadata['doc_level']
-                print(f"doc_level: {doc_level}, parent_doc_id: {parent_doc_id}")
-                
-                if parent_doc_id in docList:
-                    print('duplicated!')
-                else:
-                    relevant_documents.append(re)
-                    docList.append(parent_doc_id)
+                if 'parent_doc_id' in re[0].metadata:
+                    parent_doc_id = re[0].metadata['parent_doc_id']
+                    doc_level = re[0].metadata['doc_level']
+                    print(f"doc_level: {doc_level}, parent_doc_id: {parent_doc_id}")
+                    
+                    if parent_doc_id in docList:
+                        print('duplicated!')
+                    else:
+                        relevant_documents.append(re)
+                        docList.append(parent_doc_id)
             print('relevant_documents: ', relevant_documents)
                 
         else:
